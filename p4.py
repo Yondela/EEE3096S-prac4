e9102d80be8fae5aadd666f23085c4246ee6e7eb
ShaneMoney <88984836+ShaneMoney@users.noreply.github.com>
2021-10-04 18:07:07 +0200

    Working Code (Hopefully)

--- p4.py
+++ p4.py
@@ -3,84 +3,91 @@ import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
-
+import datetime
+import startTime
 import threading
 import time
 import RPi.GPIO as GPIO
 
-runtime_interval = [10, 5, 1]
-current_runtime_interval = 0
-btn_toggle = 37
-
-
-volt_output_0_degree = 500*10**(-3)
-temp_coefficient = 10**(-3)
-
-# create the spi bus
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-
-# create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D5)
-
-# create the mcp object
-mcp = MCP.MCP3008(spi, cs)
-
-# create an analog input channel on pin 0
-chan0 = AnalogIn(mcp, MCP.P0)
-chan1 = AnalogIn(mcp, MCP.P1)
+global chan1
+global chan2 
+global volt_output_0_degree = 500*10**(-3)
+global temp_coefficient = 10**(-3)
+global runtime_interval = 10 #By default
+global runcount = 0 #Counter for runtime interval incrementations
+global btn_toggle = 37
 
 def setup():
-    # This setup has to handle the button that changes the values
-    #GPIO.setmode(GPIO.BOARD)
+    global chan1
+    global chan2
+    global startTime
+    global btn_toggle
 
-    #GPIO.setup(btn_toggle, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # create the spi bus
+    spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
-    #GPIO.add_event_detect(btn_toggle, GPIO.FALLING, callback=btn_toggle_rate(), bouncetime=200)
-    pass
+    # create the cs (chip select)
+    cs = digitalio.DigitalInOut(board.D5)
 
-def fetch_sensor_vals():
-    # Fetches the sensor values.
-    #print("Raw ADC Value: ", chan0.value)
-    #print("ADC Voltage: " + str(chan0.voltage) + "V")
-    print("Raw ADC Value: ", chan1.value)
-    print("ADC Voltage: " + str(chan1.voltage) + "V")
-    #time.sleep(runtime_interval[current_runtime_interval])
+    # create the mcp object
+    mcp = MCP.MCP3008(spi, cs)
 
-    #temp = (float(chan1.voltage)-volt_output_0_degree)/(temp_coefficient)
-    #print("{}, {}, {} C, {}".format(10, chan1.value, temp, chan0.value))
-    time.sleep(1)
+    # create an analog input channel on pins 1 & 2
+    chan1 = AnalogIn(mcp, MCP.P1) #Temperature
+    chan2 = AnalogIn(mcp, MCP.P2) #Brightness
+
+    #Button set up
+    GPIO.setup(btn_toggle, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    
+    #Debouncing
+    GPIO.add_event_detect(btn_toggle, GPIO.FALLING, callback=btn_toggle_rate(), bouncetime=200)
+   
+    #Start timing
+    startTime = time.time()
     #pass
 
-def btn_toggle_rate(channel):
-    # You need to make this run on it's own thread so that when the button
-    # is pressed it will change the rate.
-    #current_runtime_interval += 1
-    #current_runtime_interval %= 3
-    pass
+def fetch_sensor_vals():
+    global chan1
+    global chan2
+    global startTime
+    global volt_output_0_degree
+    global temp_coefficient
+    global runtime_interval
+    
+    #threading
+    thread = threading.Timer(runtime_interval, fetch_sensor_vals)
+    thread.daemon = True
+    thread.start()
+    
+    # Fetches the sensor values.
+    timeNow = time.time()
+    
+    #print out values
+    tempOut = (chan1.voltage - volt_output_0_degree)/temp_coefficient
+    print(round(timeNow-starTime), 's \t \t', chan1.value, '\t \t \t', round((tempOut),1) , 'C' , '\t',chan2.value)
+    #pass
 
-
-##print("Raw ADC Value: ", chan.value)
-##print("ADC Voltage: " + str(chan.voltage) + "V")
-
-
-# create an analog input channel on pin 1
-##chan = AnalogIn(mcp, MCP.P1)
-##print("Raw ADC Value: ", chan.value)
-##print("ADC Voltage: " + str(chan.voltage) + "V")
+def btn_toggle_rate(root_path):   #COULD BE WRONG PARAMETR
+    global runtime_interval
+    global runcount
+    if runcount == 0:
+        runtime_interval = 5
+        runcount = (runcount+1)%3
+    elif runcount == 1:
+        runtime_interval = 1
+        runcount = (runcount+1)%3
+    elif runcount == 2:
+        runtime_interval = 10
+        runcount = (runcount+1)%3
+    #pass
 
 if __name__ == "__main__":
-
-    try:
-        #setup()
-        #print(1)
-        print("Runtime     Temp Reading     Temp    Light Reading")
-        while True:
-            fetch_sensor_vals()
-    except Exception as e:
-        print(e)
-        #GPIO.cleanup()
-    #finally:
-        #GPIO.cleanup()
+    setup()
+    print('Runtime \t Temp Reading \t \t Temp \t \t Light Reading')
+    fetch_sensor_vals()
+    while True:
+        pass    #Run forever and ever, Amen
+      
 
 
 
